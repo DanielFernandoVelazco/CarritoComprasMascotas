39c3eb7ab428fb90329e95be3e69a6a022b71c5c
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-04-01 20:30:35 -0500

    se normaliza repository cart

---
+++ backend_petstore/src/main/java/com/dfvc/backend_petstore/repository/CartItemRepository.java
@@ -0,0 +1,16 @@
+package com.dfvc.backend_petstore.repository;
+
+import com.dfvc.backend_petstore.model.CartItem;
+import com.dfvc.backend_petstore.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByUser(User user);
+    Optional<CartItem> findByUserAndProductId(User user, Long productId);
+    void deleteByUser(User user);
+}
