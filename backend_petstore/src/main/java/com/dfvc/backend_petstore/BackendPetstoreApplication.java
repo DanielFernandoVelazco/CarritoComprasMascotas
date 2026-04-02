4aab6254f277929effc184f64dab75984b38c818
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-04-01 19:26:09 -0500

    se normaliza back app

---
+++ backend_petstore/src/main/java/com/dfvc/backend_petstore/BackendPetstoreApplication.java
@@ -0,0 +1,13 @@
+package com.dfvc.backend_petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPetstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPetstoreApplication.class, args);
+	}
+
+}
