90b154a3c6e48c434bedc928c2c2a23979bbbbb8
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-04-01 19:25:21 -0500

    se normaliza test

---
+++ backend_petstore/src/test/java/com/dfvc/backend_petstore/BackendPetstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfvc.backend_petstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPetstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
