9b6506967bbc03628b0b12844c886094c2b2134b
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-04-06 23:19:57 -0500

    se normaliza message

---
+++ backend_petstore/src/main/java/com/dfvc/backend_petstore/dto (copy)/response/MessageResponseDTO.java
@@ -0,0 +1,12 @@
+package com.dfvc.backend_petstore.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageResponseDTO {
+    private String message;
+}
