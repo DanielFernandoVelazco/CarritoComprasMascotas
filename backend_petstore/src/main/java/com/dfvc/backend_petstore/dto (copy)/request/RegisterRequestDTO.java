b80b429155bdb160e609863ee5ec0f69bcc47603
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-04-06 23:18:45 -0500

    se normaliza register

---
+++ backend_petstore/src/main/java/com/dfvc/backend_petstore/dto (copy)/request/RegisterRequestDTO.java
@@ -0,0 +1,26 @@
+package com.dfvc.backend_petstore.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequestDTO {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    private String phoneNumber;
+}
