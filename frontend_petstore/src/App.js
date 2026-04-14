725a67c9653d58d699a21e43713d3da626d4d6bf
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-04-13 23:12:01 -0500

    se normaliza app

--- frontend_petstore/src/App.js
+++ frontend_petstore/src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import BottomNavBar from './components/layout/BottomNavBar';
+import PrivateRoute from './components/common/PrivateRoute';
+
+// Pages
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import OrderSuccessPage from './pages/OrderSuccessPage';
+import OrderHistoryPage from './pages/OrderHistoryPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <AuthProvider>
+          <CartProvider>
+            <div className="min-h-screen flex flex-col">
+              <Navbar />
+              <main className="flex-grow pb-20 md:pb-0">
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/register" element={<RegisterPage />} />
+                  <Route path="/products" element={<HomePage />} />
+                  <Route path="/product/:id" element={<ProductDetailPage />} />
+                  <Route path="/cart" element={<CartPage />} />
+                  <Route
+                      path="/checkout"
+                      element={
+                        <PrivateRoute>
+                          <CheckoutPage />
+                        </PrivateRoute>
+                      }
+                  />
+                  <Route
+                      path="/order-success/:orderNumber"
+                      element={
+                        <PrivateRoute>
+                          <OrderSuccessPage />
+                        </PrivateRoute>
+                      }
+                  />
+                  <Route
+                      path="/orders"
+                      element={
+                        <PrivateRoute>
+                          <OrderHistoryPage />
+                        </PrivateRoute>
+                      }
+                  />
+                </Routes>
+              </main>
+              <Footer />
+              <BottomNavBar />
+            </div>
+          </CartProvider>
+        </AuthProvider>
+      </Router>
   );
 }
 
